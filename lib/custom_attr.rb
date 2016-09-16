e065434bd142c0827fff61ddcb56912e13fecb34
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-16 16:40:13 -0400

    Add comments on class vs instance methods
    define_method

--- lib/custom_attr.rb
+++ lib/custom_attr.rb
@@ -2,7 +2,13 @@
 class Bar
   # we have to define this method on `self` (see below comment)
   def self.attribute_reader(attribute)
+    # define_method defines an INSTANCE method on the receiver
+    # the receiver in this case is the class Bar
+    # the method is defined on instances of bar
+    # define_method is same inside a class as def methodname
     define_method attribute do
+      # self here is the instance
+      puts "self is #{self}"
       self.instance_variable_get("@#{attribute}")
     end
   end
@@ -19,6 +25,12 @@ class Bar
   end
 
   # these methods are executed within the definition of the Bar class
+
+  # attribute_reader :foo behaves like this:
+  # def foo
+  #   @foo
+  # end
+  # attribute_reader is a method call on the class
   attribute_reader :foo
   attribute_writer :bar
   attribute_accessor :baz
